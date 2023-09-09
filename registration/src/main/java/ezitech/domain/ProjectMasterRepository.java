b8068811d4f8cb42e9a32604a4aa173905a13fbe
Jinyoung Jang <jyjang@uengine.org>
2023-09-09 11:50:08 +0000

    aggregate repo 에 대한 잘못된 정보 여전

--- registration/src/main/java/ezitech/domain/ProjectMasterRepository.java
+++ registration/src/main/java/ezitech/domain/ProjectMasterRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //<<< PoEAA / Repository
 @RepositoryRestResource(collectionResourceRel="projectMasters", path="projectMasters")
-public interface ProjectMasterRepository extends PagingAndSortingRepository<ProjectMaster, >{
+public interface ProjectMasterRepository extends PagingAndSortingRepository<ProjectMaster, String>{
 }
