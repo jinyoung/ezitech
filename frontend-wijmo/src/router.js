aab52d79472d6b22c28af6c23c9af6d4aacf70ec
Jinyoung Jang <jyjang@uengine.org>
2023-09-09 08:31:49 +0000

    c

--- frontend-wijmo/src/router.js
+++ frontend-wijmo/src/router.js
@@ -5,16 +5,13 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 
-import Manager from "./components/ui/Grid"
 import ProjectMasterManager from "./components/ui/ProjectMasterGrid"
 
 
 
 
-import Manager from "./components/ui/Grid"
 import BudgetManager from "./components/ui/BudgetGrid"
 
-import Manager from "./components/ui/Grid"
 import ResearcherManager from "./components/ui/ResearcherGrid"
 
 
@@ -23,11 +20,6 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [
             {
-                path: '/',
-                name: 'Manager',
-                component: Manager
-            },
-            {
                 path: '/projectMasters',
                 name: 'ProjectMasterManager',
                 component: ProjectMasterManager
@@ -35,23 +27,12 @@ export default new Router({
 
 
 
-
-            {
-                path: '/',
-                name: 'Manager',
-                component: Manager
-            },
             {
                 path: '/budgets',
                 name: 'BudgetManager',
                 component: BudgetManager
             },
 
-            {
-                path: '/',
-                name: 'Manager',
-                component: Manager
-            },
             {
                 path: '/researchers',
                 name: 'ResearcherManager',
